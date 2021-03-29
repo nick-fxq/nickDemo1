02e997c8bf6dc175b26ccbd65db3beef9dd913a3
Nick <nick_fxq@163.com>
2021-03-29 14:59:07 +0800

    第一次更新git

--- src/com/nick/demo/Demo1.java
+++ src/com/nick/demo/Demo1.java
@@ -3,6 +3,6 @@ package com.nick.demo;
 public class Demo1 {
     public static void main(String[] args) {
         System.out.println("2222");
-        System.out.println("Hellogit");
+        System.out.println("第二次。。。。");
     }
 }
